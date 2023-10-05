6788f999db5592e67a58e017fc7f030b0364711c
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-10-05 09:31:55 -0400

    Fix cloudflare pages config

--- bankless-community/cloudflare/pages.tf
+++ bankless-community/cloudflare/pages.tf
@@ -1,29 +1,25 @@
 resource "cloudflare_pages_project" "deployment_configs" {
-  account_id        = "f037e56e89293a057740de681ac9abbe"
-  name              = "this-is-my-project-01"
+  account_id        = local.cloudflare_account_id
+  name              = local.cloudflare_account_name
   production_branch = "main"
 
   source {
     type = "github"
     config {
-      owner                         = "cloudflare"
-      repo_name                     = "ninjakittens"
+      owner                         = local.cloudflare_account_name
+      repo_name                     = "bankless-website"
       production_branch             = "main"
       pr_comments_enabled           = true
       deployments_enabled           = true
       production_deployment_enabled = true
-      preview_deployment_setting    = "custom"
-      preview_branch_includes       = ["dev", "preview"]
-      preview_branch_excludes       = ["main", "prod"]
+      preview_deployment_setting    = "all"
     }
   }
 
   build_config {
-    build_command       = "npm run build"
-    destination_dir     = "build"
+    build_command       = "npx next build"
+    destination_dir     = "out"
     root_dir            = ""
-    web_analytics_tag   = "cee1c73f6e4743d0b5e6bb1a0bcaabcc"
-    web_analytics_token = "021e1057c18547eca7b79f2516f06o7x"
   }
 
   deployment_configs {
@@ -32,50 +28,24 @@ resource "cloudflare_pages_project" "deployment_configs" {
         ENVIRONMENT = "preview"
       }
       secrets = {
-        TURNSTILE_SECRET = "1x0000000000000000000000000000000AA"
+        NEXT_PUBLIC_ALCHEMY_API_KEY = "xoSmxF5VP5yJq0zV-MedaxZUCYSt7DUN"
+        NEXT_PUBLIC_BLOCKNATIVE_API_KEY = "9176eee3-12fa-431c-93c5-27d1f40d4c91"
+        NEXT_PUBLIC_INFURA_PROJECT_ID = "9344a875cace468ca6c8029147cda56a"
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID = "e1726502f108ccc7845fc598e1a605f3"
       }
-      kv_namespaces = {
-        KV_BINDING = "5eb63bbbe01eeed093cb22bb8f5acdc3"
-      }
-      durable_object_namespaces = {
-        DO_BINDING = "5eb63bbbe01eeed093cb22bb8f5acdc3"
-      }
-      r2_buckets = {
-        R2_BINDING = "some-bucket"
-      }
-      d1_databases = {
-        D1_BINDING = "445e2955-951a-4358-a35b-a4d0c813f63"
-      }
-      compatibility_date  = "2022-08-15"
-      compatibility_flags = ["nodejs_compat"]
+      always_use_latest_compatibility_date = true
     }
     production {
       environment_variables = {
         ENVIRONMENT = "production"
-        OTHER_VALUE = "other value"
       }
       secrets = {
-        TURNSTILE_SECRET       = "1x0000000000000000000000000000000AA"
-        TURNSTILE_INVIS_SECRET = "2x0000000000000000000000000000000AA"
+        NEXT_PUBLIC_ALCHEMY_API_KEY = "xoSmxF5VP5yJq0zV-MedaxZUCYSt7DUN"
+        NEXT_PUBLIC_BLOCKNATIVE_API_KEY = "9176eee3-12fa-431c-93c5-27d1f40d4c91"
+        NEXT_PUBLIC_INFURA_PROJECT_ID = "9344a875cace468ca6c8029147cda56a"
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID = "e1726502f108ccc7845fc598e1a605f3"
       }
-      kv_namespaces = {
-        KV_BINDING_1 = "5eb63bbbe01eeed093cb22bb8f5acdc3"
-        KV_BINDING_2 = "3cdca5f8bb22bc390deee10ebbb36be5"
-      }
-      durable_object_namespaces = {
-        DO_BINDING_1 = "5eb63bbbe01eeed093cb22bb8f5acdc3"
-        DO_BINDING_2 = "3cdca5f8bb22bc390deee10ebbb36be5"
-      }
-      r2_buckets = {
-        R2_BINDING_1 = "some-bucket"
-        R2_BINDING_2 = "other-bucket"
-      }
-      d1_databases = {
-        D1_BINDING_1 = "445e2955-951a-4358-a35b-a4d0c813f63"
-        D1_BINDING_2 = "a399414b-c697-409a-a688-377db6433cd9"
-      }
-      compatibility_date  = "2022-08-16"
-      compatibility_flags = ["nodejs_compat", "streams_enable_constructors"]
+      always_use_latest_compatibility_date = true
     }
   }
 }
