051beaa8fd828145a97a5dddc96f7a01b979e839
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-10-25 08:17:28 -0400

    feat: deploy bankless.community to cloudflare pages (#27)
    * Add cloudflare pages
    * Fix README (there is no Makefile so `make` doesn't work)
    * Fix cloudflare pages formatting using pre-commit
    * Convert plaintext secrets to senstive variables

---
+++ bankless-community/cloudflare/pages.tf
@@ -0,0 +1,51 @@
+resource "cloudflare_pages_project" "deployment_configs" {
+  account_id        = local.cloudflare_account_id
+  name              = local.cloudflare_account_name
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = local.cloudflare_account_name
+      repo_name                     = "bankless-website"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "all"
+    }
+  }
+
+  build_config {
+    build_command   = "npx next build"
+    destination_dir = "out"
+    root_dir        = ""
+  }
+
+  deployment_configs {
+    preview {
+      environment_variables = {
+        ENVIRONMENT = "preview"
+      }
+      secrets = {
+        NEXT_PUBLIC_ALCHEMY_API_KEY          = var.alchemy_api_key
+        NEXT_PUBLIC_BLOCKNATIVE_API_KEY      = var.blocknative_api_key
+        NEXT_PUBLIC_INFURA_PROJECT_ID        = var.infura_project_id
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID = var.walletconnect_project_id
+      }
+      always_use_latest_compatibility_date = true
+    }
+    production {
+      environment_variables = {
+        ENVIRONMENT = "production"
+      }
+      secrets = {
+        NEXT_PUBLIC_ALCHEMY_API_KEY          = var.alchemy_api_key
+        NEXT_PUBLIC_BLOCKNATIVE_API_KEY      = var.blocknative_api_key
+        NEXT_PUBLIC_INFURA_PROJECT_ID        = var.infura_project_id
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID = var.walletconnect_project_id
+      }
+      always_use_latest_compatibility_date = true
+    }
+  }
+}
--- bankless-community/cloudflare/variables.tf
+++ bankless-community/cloudflare/variables.tf
@@ -1,3 +1,7 @@
 variable "cloudflare_api_token" {
   sensitive = true
 }
+variable "alchemy_api_key" { sensitive = true }
+variable "blocknative_api_key" { sensitive = true }
+variable "infura_project_id" { sensitive = true }
+variable "walletconnect_project_id" { sensitive = true }
